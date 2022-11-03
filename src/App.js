31956fa4251504fd3117ad7e0d930a95d9614b73
VyacheslavDef <slava_chetvertakov@mail.ru>
2022-11-03 22:43:17 +0600

    clear front

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import React from "react";
+import TittlePage from "./components/TittlePage";
 
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <>
+      <TittlePage/>
+    </>
   );
 }
 
