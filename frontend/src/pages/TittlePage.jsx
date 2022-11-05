86f244858bbaf84e952fef228f1101c9c6a050ba
VyacheslavDef <slava_chetvertakov@mail.ru>
2022-11-05 21:25:01 +0600

    itemcard and styles

--- frontend/src/pages/TittlePage.jsx
+++ frontend/src/pages/TittlePage.jsx
@@ -1,6 +1,21 @@
 import React from 'react'
+import { useState } from 'react'
+import ItemCard from '../components/ItemCard'
 
 const TittlePage = () => {
+
+  const [products, setProducts] = useState([
+    {id:1, title: 'item 1', price: '324$', disc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis ratione aperiam quisquam corporis'},
+    {id:2, title: 'item 2', price: '324$', disc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis ratione aperiam quisquam corporis'},
+    {id:3, title: 'item 3', price: '324$', disc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis ratione aperiam quisquam corporis'},
+    {id:4, title: 'item 4', price: '324$', disc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis ratione aperiam quisquam corporis'},
+    {id:5, title: 'item 5', price: '324$', disc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis ratione aperiam quisquam corporis'},
+    {id:6, title: 'item 6', price: '324$', disc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis ratione aperiam quisquam corporis'},
+    {id:7, title: 'item 7', price: '324$', disc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis ratione aperiam quisquam corporis'},
+    {id:8, title: 'item 8', price: '324$', disc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis ratione aperiam quisquam corporis'},
+    {id:9, title: 'item 9', price: '324$', disc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis ratione aperiam quisquam corporis'},
+    {id:10, title: 'item 10', price: '324$', disc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis ratione aperiam quisquam corporis'},
+  ])
   return (
     <>
       <div className='container'>
@@ -13,8 +28,11 @@ const TittlePage = () => {
 
       <div className="container">
         <div className='main_products_container'>
+          <div className="main_disc_container">
+            <h2>Популярные товары: </h2>
+          </div>
           <div className='product_wrapper'>
-            <h1>Products</h1>
+            {products.map((x)=> (<ItemCard item={x} key={x.id}/>))}
           </div>
         </div>
       </div>
