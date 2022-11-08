6eaa7f1608ec4c3b47453113c556f6e62d01b720
VyacheslavDef <slava_chetvertakov@mail.ru>
2022-11-08 20:20:04 +0600

    api test

--- frontend/src/components/API/PostService.jsx
+++ frontend/src/components/API/PostService.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default class PostService {
     static async getAll () {
-        const responce = await axios.get('http://127.0.0.1:8000/api/products')
+        const responce = await axios.get('https://jsonplaceholder.typicode.com/posts')
         return responce
     }
 }
--- frontend/src/pages/TittlePage.jsx
+++ frontend/src/pages/TittlePage.jsx
@@ -5,7 +5,7 @@ import ItemCard from '../components/ItemCard'
 
 const TittlePage = () => {
 
-  const [products, setProducts] = useState([
+  const [products] = useState([
     {id:1, title: 'item 1', price: '324$', disc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis ratione aperiam quisquam corporis'},
     {id:2, title: 'item 2', price: '324$', disc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis ratione aperiam quisquam corporis'},
     {id:3, title: 'item 3', price: '324$', disc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis ratione aperiam quisquam corporis'},
@@ -18,17 +18,17 @@ const TittlePage = () => {
   async function getProd () {
     const responce = await PostService.getAll()
     setProd(responce)
+    console.log(newProd)
   }
-  useEffect(() => {
-  getProd()
-  }, [])
-  console.log(newProd)
+
+
   return (
     <>
       <div className='container'>
         <div className='title_container'>
           <div className='title_text_container'>
             <h1>Tittle Name</h1>
+            <button onClick={getProd}>APITEST</button>
           </div>
         </div>
       </div>
